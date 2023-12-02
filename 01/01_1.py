983fccf1f31101cc13272f6587f461c1b6a902db
DGoblot <90686224+DGoblot@users.noreply.github.com>
2023-12-02 14:01:17 +0100

    Seperated parts 1 & 2

--- 01/01_1.py
+++ 01/01_1.py
@@ -3,20 +3,8 @@ import re
 input_file = open("01_input.txt", "r")
 input = input_file.readlines()
 res = 0
-letters_dict = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
 
 for line in input:
-    index_start = 0
-    index_end = 0
-    for letters in letters_dict:
-        index_start = line.find(letters)
-        if index_start != -1:
-            line = line[:index_start+1] + letters_dict[letters] + line[index_start+1:]
-
-        index_end = line.rfind(letters)
-        if index_end != -1:
-            line = line[:index_end+1] + letters_dict[letters] + line[index_end+1:]
-
     digits = re.findall(r"\d", line)
     res_line = int(digits[0]+digits[-1])
     res += res_line
