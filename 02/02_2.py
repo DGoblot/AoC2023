4bd6f9c9b104745081b92c20d4022bd36b25302c
DGoblot <90686224+DGoblot@users.noreply.github.com>
2023-12-02 14:51:56 +0100

    Day 2

---
+++ 02/02_2.py
@@ -0,0 +1,28 @@
+input_file = open("02_input.txt", "r")
+input = input_file.readlines()
+res = 0
+
+max_r, max_g, max_b = 12, 13, 14
+
+for line in input:
+    r, g, b = 0, 0, 0
+    line = line.replace(':', '')
+    line = line.replace(',', '')
+    line = line.replace(';', '')
+    line = line.split()
+    
+    for i, word in enumerate(line):
+        if word == 'red':
+            if r < int(line[i-1]):
+                r = int(line[i-1])
+        if word == 'green':
+            if g < int(line[i-1]):
+                g = int(line[i-1])
+        if word == 'blue':
+            if b < int(line[i-1]):
+                b = int(line[i-1])
+        
+    game_res = r*g*b
+    res += game_res
+
+print(res)
